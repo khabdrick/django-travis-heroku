0d82e13762fd7c0af8a0c554c431ba01f6f7b261
Muhammed Ali <muhamzyali@gmail.com>
2022-09-19 17:19:17 +0100

    first commit

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import RecipeListSerializer
+from rest_framework.response import Response
+from .models import RecipeModel
+
+@api_view(['POST'])
+def create_recipe(request):
+        # create coontact
+    recipe = RecipeListSerializer(data=request.data)
+    if recipe.is_valid():
+        recipe.save()
+        return Response(recipe.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class RecipeListView(generics.ListCreateAPIView):
+    # list out contacts
+    queryset = RecipeModel.objects.all()
+    serializer_class = RecipeListSerializer
